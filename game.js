710908c527c7396b02164505ce24e5997938c120
Jordan Perkins <jordanperkins@hotmail.co.uk>
2021-03-23 20:22:58 +0000

    added loader, and fixed api issue

--- game.js
+++ game.js
@@ -3,6 +3,9 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById("progressBarFull")
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
+
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -37,14 +40,16 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
 
 //Constants
 
-const CORRECT_BONUS = 10
-const MAX_QUESTIONS = 3;
+const CORRECT_BONUS = 5;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions]
     getNewQuestion();
+    loader.classList.add("hidden")
+    game.classList.remove("hidden");
 }
 
 getNewQuestion = () => {
@@ -62,11 +67,11 @@ getNewQuestion = () => {
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+    question.innerHTML = currentQuestion.question;
 
     choices.forEach( choice => {
         const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
+        choice.innerHTML = currentQuestion['choice' + number];
     });
 
     availableQuestions.splice(questionIndex,1);
